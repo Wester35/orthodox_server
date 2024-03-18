13090b0cf27f8e2cafac2c46da2484770b7d3e29
Wester35 <shum0302@gmail.com>
2024-03-18 14:56:55 +0300

    db added (first phase)

--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@5.228.52.190/from_flask'
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
 
 @app.route('/')
+@app.route('/home')
 def index():
     return "Welcome to the main page"
 
@@ -15,10 +27,9 @@ def registration_page():
 
 @app.route('/register', methods=['POST'])
 def register():
-    # Здесь будет ваша логика обработки регистрации
     username = request.form['username']
     password = request.form['password']
-    # Здесь будет код для работы с базой данных
+
     return f"User {username} registered successfully!"
 
 
@@ -29,10 +40,33 @@ def login_page():
 
 @app.route('/sign_in', methods=['POST'])
 def login():
-    # Здесь будет ваша логика аутентификации
     username = request.form['username']
     password = request.form['password']
+
     return f"User {username} logged in successfully!"
 
+
+# @app.route('/api/endpoint/')
+# def process_request():
+#     data = request.json
+#     if 'usrName' in data and 'usrPasswd' in data:
+#         return jsonify(data)
+#     else:
+#         # Если какое-то поле отсутствует, возвращаем ошибку
+#         return jsonify({'error': 'Missing fields in request'}), 400
+@app.route('/api/endpoint/', methods=['POST'])
+def process_data():
+    # Получаем данные из тела POST-запроса
+    user_name = request.form.get('usrName')
+    password = request.form.get('usrPasswd')
+
+    # Обработка полученных данных
+    # Например, можно отправить их обратно в виде JSON-ответа
+    return jsonify({
+        'status': 'success',
+        'userName': user_name,
+        'password': password
+    })
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=7865, debug=True)
