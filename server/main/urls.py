c5caf5f1972012ecec2ca1590148571a471aeb8c
Wester35 <shum0302@gmail.com>
2024-03-23 19:04:22 +0300

    0 1 1

--- server/main/urls.py
+++ server/main/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('auth', views.auth, name='auth'),
+    path('register', views.register, name='register'),
 ]
--- server/main/views.py
+++ server/main/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("<h1>Hello, world. You're at the orthodox server!</h1>")
+
+
+def auth(request):
+    return render(request, 'main/sign_in.html')
+
+
+def register(request):
+    return render(request, 'main/sign_up.html')
+
